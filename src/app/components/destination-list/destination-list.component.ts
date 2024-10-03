3379264ce9a5b60e564ae5d2683e4106805666cf
Darsssiiieee <darsie040@gmail.com>
2024-10-04 02:05:53 +0800

    implementation of destination list

--- src/app/components/destination-list/destination-list.component.ts
+++ src/app/components/destination-list/destination-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import DestinationList from '../interface/destination-list';
 
 @Component({
   selector: 'app-destination-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './destination-list.component.css'
 })
 export class DestinationListComponent {
+  destinations: DestinationList[] = [];
 
+	id: number = 0;
+	name: string = '';
+	description: string = '';
+	location: string = '';
+
+	addDestination() { if (this.name,this.description,this.id,this.location.trim())
+		this.destinations.push({
+			id: this.id,
+			name: this.name,
+			description: this.description,
+			location: this.location,
+		});
+
+		this.id = 0;
+		this.name = '';
+		this.description = '';
+		this.location = '';
+	}
 }
