c15d1b98956e0c0d9cd529a1189fe89616b5adb0
Darsssiiieee <darsie040@gmail.com>
2024-10-04 03:27:06 +0800

    implementation of event list

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import EventList from '../interface/event-list';
 
 @Component({
   selector: 'app-event-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './event-list.component.css'
 })
 export class EventListComponent {
+  events: EventList[] = [];
 
+	eventDate: string = '';
+	eventName: string = '';
+	eventLocation: string = '';
+	eventPrice: number = 0;
+	eventDuration: string = '';
+
+	addEvent() {
+		this.events.push({
+			eventDate: this.eventDate,
+			eventName: this.eventName,
+			eventLocation: this.eventLocation,
+			eventPrice: this.eventPrice,
+			eventDuration: this.eventDuration,
+		});
+
+		this.eventDate = '';
+		this.eventName = '';
+		this.eventLocation = '';
+		this.eventPrice = 0;
+		this.eventDuration = '';
+	}
 }
