f2ba5679767c0cf9b1c99b8b6e3e590da449ff35
Darsssiiieee <darsie040@gmail.com>
2024-10-03 23:26:14 +0800

    implementation of building list

--- src/app/components/building-list/building-list.component.ts
+++ src/app/components/building-list/building-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import BuildingList from '../interface/building-list';
 
 @Component({
   selector: 'app-building-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './building-list.component.css'
 })
 export class BuildingListComponent {
+  buildingList: BuildingList[] = [];
 
+	name: string = '';
+	address: string = '';
+	floors: number = 0;
+	rooms: number = 0;
+	occupants: number = 0;
+
+	addBuilding() { if (this.name,this.floors,this.rooms,this.occupants,this.address.trim())
+		this.buildingList.push({
+			name: this.name,
+			address: this.address,
+			floors: this.floors,
+			rooms: this.rooms,
+			occupants: this.occupants,
+		});
+
+		this.name = '';
+		this.address = '';
+		this.floors = 0;
+		this.rooms = 0;
+		this.occupants = 0;
+	}
 }
