37785c74e10dbe621d37ab04c8dfcac9c211b24e
Darsssiiieee <darsie040@gmail.com>
2024-10-04 04:39:18 +0800

    implementation of fruit list

--- src/app/components/fruit-list/fruit-list.component.ts
+++ src/app/components/fruit-list/fruit-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import FruitList from '../interface/fruit-list';
 
 @Component({
   selector: 'app-fruit-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './fruit-list.component.css'
 })
 export class FruitListComponent {
+	fruits: FruitList[] = [];
 
+	id: string = '';
+	name: string = '';
+	quantity: number = 0;
+	price: number = 0;
+
+	addFruit(): void { if (this.name,this.quantity,this.price,this.id.trim())
+		this.fruits.push({
+			id: this.id,
+			name: this.name,
+			quantity: this.quantity,
+			price: this.price,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.quantity = 0;
+		this.price = 0;
+	}
 }
