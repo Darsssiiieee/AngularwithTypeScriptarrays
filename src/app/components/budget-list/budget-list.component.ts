b094759da014d26260b9bb0c8a677702d28e2200
Darsssiiieee <darsie040@gmail.com>
2024-10-04 02:19:00 +0800

    implementation of budget list

--- src/app/components/budget-list/budget-list.component.ts
+++ src/app/components/budget-list/budget-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import BudgetList from '../interface/budget-list';
 
 @Component({
   selector: 'app-budget-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './budget-list.component.css'
 })
 export class BudgetListComponent {
+	budgets: BudgetList[] = [];
 
+	project: string = '';
+	budget: number = 0;
+	status: string = '';
+	startDate: string = '';
+	endDate: string = '';
+
+	addBudget() { if (this.project,this.budget,this.status,this.startDate,this.endDate.trim())
+		this.budgets.push({
+			project: this.project,
+			budget: this.budget,
+			status: this.status,
+			startDate: this.startDate,
+			endDate: this.endDate,
+		});
+
+		this.project = '';
+		this.budget = 0;
+		this.status = '';
+		this.startDate = '';
+		this.endDate = '';
+	}
 }
