3e63a6c0bcca93797544f0a389c84ae2b00ef50a
Darsssiiieee <darsie040@gmail.com>
2024-10-03 22:25:47 +0800

    implementation of artist list

--- src/app/components/artist-list/artist-list.component.ts
+++ src/app/components/artist-list/artist-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ArtistList from '../interface/artist-list';
 
 @Component({
   selector: 'app-artist-list',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './artist-list.component.css'
 })
 export class ArtistListComponent {
+  artistList: ArtistList[] = [];
 
+	name: string = '';
+	field: string = '';
+	genre: string = '';
+	country: string = '';
+
+	addArtist() { if (this.name,this.field,this.genre,this.country.trim())
+		this.artistList.push({
+			name: this.name,
+			field: this.field,
+			genre: this.genre,
+			country: this.country,
+		});
+		this.name = '';
+		this.field = '';
+		this.genre = '';
+		this.country = '';
+	}
 }
