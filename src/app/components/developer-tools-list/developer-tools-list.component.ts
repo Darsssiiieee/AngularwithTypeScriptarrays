44941c49f14ac71322c89556baae0100a6f09eab
Darsssiiieee <darsie040@gmail.com>
2024-10-04 02:59:37 +0800

    implementation of developer tools list

--- src/app/components/developer-tools-list/developer-tools-list.component.ts
+++ src/app/components/developer-tools-list/developer-tools-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import DeveloperToolsList from '../interface/developer-tools-list';
 
 @Component({
   selector: 'app-developer-tools-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './developer-tools-list.component.css'
 })
 export class DeveloperToolsListComponent {
+  developerTools: DeveloperToolsList[] = [];
 
+	toolName: string = '';
+	toolDescription: string = '';
+	toolPrice: number = 0;
+	toolDuration: string = '';
+
+	addDeveloperTool() { if (this.toolName,this.toolDescription,this.toolPrice,this.toolDuration.trim())
+		this.developerTools.push({
+			toolName: this.toolName,
+			toolDescription: this.toolDescription,
+			toolPrice: this.toolPrice,
+			toolDuration: this.toolDuration,
+		});
+
+		this.toolName = '';
+		this.toolDescription = '';
+		this.toolPrice = 0;
+		this.toolDuration = '';
+	}
 }
