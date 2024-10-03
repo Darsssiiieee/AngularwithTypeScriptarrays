240090204dcb391159752f5c343043caf061ca9d
Darsssiiieee <darsie040@gmail.com>
2024-10-04 00:30:03 +0800

    implementation of computer hardware list

--- src/app/components/computer-hardware-list/computer-hardware-list.component.ts
+++ src/app/components/computer-hardware-list/computer-hardware-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ComputerHardware from '../interface/computer-hardware-list';
 
 @Component({
   selector: 'app-computer-hardware-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './computer-hardware-list.component.css'
 })
 export class ComputerHardwareListComponent {
+  computerHardware: ComputerHardware[] = [];
 
+	name: string = '';
+	location: string = '';
+	type: string = '';
+	price: number = 0;
+
+	addComputerHardware() { if (this.name,this.type,this.price,this.location.trim())
+		this.computerHardware.push({
+			name: this.name,
+			location: this.location,
+			type: this.type,
+			price: this.price,
+		});
+
+		this.name = '';
+		this.location = '';
+		this.type = '';
+		this.price = 0;
+	}
 }
