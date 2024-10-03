3454408fada7222ac2f8ff09f15f4f98ab844c69
Darsssiiieee <darsie040@gmail.com>
2024-10-04 04:53:43 +0800

    implementation of furniture list

--- src/app/components/furniture-list/furniture-list.component.ts
+++ src/app/components/furniture-list/furniture-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import FurnitureList from '../interface/furniture-list';
 
 @Component({
   selector: 'app-furniture-list',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './furniture-list.component.css'
 })
 export class FurnitureListComponent {
+  furnitureList: FurnitureList[] = [];
 
+	name: string = '';
+	description: string = '';
+	type: string = '';
+	price: number = 0;
+	category: string = '';
+
+	addFurniture() { if (this.name,this.price,this.type,this.category,this.description.trim())
+		this.furnitureList.push({
+			name: this.name,
+			description: this.description,
+			type: this.type,
+			price: this.price,
+			category: this.category,
+		});
+		this.name = '';
+		this.description = '';
+		this.type = '';
+		this.price = 0;
+		this.category = '';
+	}
 }
