92867fbbb2364950642236bcf662da32d6a382fc
Darsssiiieee <darsie040@gmail.com>
2024-10-03 23:45:04 +0800

    implementation of car model list

--- src/app/components/car-model-list/car-model-list.component.ts
+++ src/app/components/car-model-list/car-model-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import CarModel from '../interface/car-model';
 
 @Component({
   selector: 'app-car-model-list',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './car-model-list.component.css'
 })
 export class CarModelListComponent {
+  carModels: CarModel[] = [];
+  
+	id: string = '';
+	name: string = '';
+	year: number = 0;
+	make: string = '';
+	model: string = '';
+	price: number = 0;
 
+	addCarModel(): void { if (this.name,this.id,this.year,this.price,this.model,this.make.trim())
+		this.carModels.push({
+			id: Number(this.id),
+			name: this.name,
+			year: this.year,
+			make: this.make,
+			model: this.model,
+			price: this.price,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.year = 0;
+		this.make = '';
+		this.model = '';
+		this.price = 0;
+	}
 }
