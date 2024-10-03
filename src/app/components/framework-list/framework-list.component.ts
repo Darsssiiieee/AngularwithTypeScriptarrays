c30b9c7a02f85078ebfd93c8bb7275cbb37592e8
Darsssiiieee <darsie040@gmail.com>
2024-10-04 04:24:40 +0800

    implementation of framework list

--- src/app/components/framework-list/framework-list.component.ts
+++ src/app/components/framework-list/framework-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import FrameworkList from '../interface/framework-list';
 
 @Component({
   selector: 'app-framework-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './framework-list.component.css'
 })
 export class FrameworkListComponent {
+  frameworks: FrameworkList[] = [];
 
+	name: string = '';
+	description: string = '';
+	developedBy: string = '';
+	firstRelease: string = '';
+	latestRelease: string = '';
+
+	addFramework() { if (this.name,this.description,this.developedBy,this.firstRelease,this.latestRelease.trim())
+		this.frameworks.push({
+			name: this.name,
+			description: this.description,
+			developedBy: this.developedBy,
+			firstRelease: this.firstRelease,
+			latestRelease: this.latestRelease,
+		});
+
+		this.name = '';
+		this.description = '';
+		this.developedBy = '';
+		this.firstRelease = '';
+		this.latestRelease = '';
+	}
 }
