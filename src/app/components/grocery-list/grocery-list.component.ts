9ee41747ed5298783c8252b4eba5a39eaa013e50
Darsssiiieee <darsie040@gmail.com>
2024-10-04 05:25:15 +0800

    implementation of grocery list

--- src/app/components/grocery-list/grocery-list.component.ts
+++ src/app/components/grocery-list/grocery-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import GroceryList from '../interface/grocery-list';
 
 @Component({
   selector: 'app-grocery-list',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './grocery-list.component.css'
 })
 export class GroceryListComponent {
+  groceryList: GroceryList[] = [];
 
+	id: number = 0;
+	name: string = '';
+	price: number = 0;
+	description: string = '';
+
+	addGrocery() {if (this.name,this.id,this.price,this.description.trim())
+		this.groceryList.push({
+			id: this.id,
+			name: this.name,
+			price: this.price,
+			description: this.description,
+		});
+		this.id = 0;
+		this.name = '';
+		this.price = 0;
+		this.description = '';
+	}
 }
