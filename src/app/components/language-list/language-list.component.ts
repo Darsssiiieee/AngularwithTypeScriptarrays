23e8c6d58fe5e9d053cb7e11a03125eec27f0907
Darsssiiieee <darsie040@gmail.com>
2024-10-04 06:00:09 +0800

    implementation of language list

--- src/app/components/language-list/language-list.component.ts
+++ src/app/components/language-list/language-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import LanguageList from '../interface/language-list';
 
 @Component({
   selector: 'app-language-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './language-list.component.css'
 })
 export class LanguageListComponent {
+  languages: LanguageList[] = [];
 
+	id: string = '';
+	name: string = '';
+	description: string = '';
+	origin: string = '';
+
+	addLanguage(): void {if (this.name,this.id,this.description,this.origin.trim())
+		this.languages.push({
+			id: Number(this.id),
+			name: this.name,
+			description: this.description,
+			origin: this.origin,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.description = '';
+		this.origin = '';
+	}
 }
