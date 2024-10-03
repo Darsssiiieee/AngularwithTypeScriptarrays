f09c7d28da993a91c79ddb5bf761406037b5ad63
Darsssiiieee <darsie040@gmail.com>
2024-10-03 23:53:43 +0800

    implementation of city list

--- src/app/components/city-list/city-list.component.ts
+++ src/app/components/city-list/city-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import CityList from '../interface/city-list';
 
 @Component({
   selector: 'app-city-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './city-list.component.css'
 })
 export class CityListComponent {
+  cities: CityList[] = [];
 
+	id: string = '';
+	name: string = '';
+	country: string = '';
+	population: number = 0;
+
+	addCity(): void { if (this.name,this.country,this.population,this.id.trim())
+		this.cities.push({
+			id: this.id,
+			name: this.name,
+			country: this.country,
+			population: this.population,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.country = '';
+		this.population = 0;
+	}
 }
