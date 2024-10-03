8f459f64266d982aa6390c82dd8dd49ae511ffb8
Darsssiiieee <darsie040@gmail.com>
2024-10-04 04:07:11 +0800

    implementation of food menu list

--- src/app/components/food-menu-list/food-menu-list.component.ts
+++ src/app/components/food-menu-list/food-menu-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import FoodMenuList from '../interface/food-menu-list';
 
 @Component({
   selector: 'app-food-menu-list',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './food-menu-list.component.css'
 })
 export class FoodMenuListComponent {
+  foodMenuList: FoodMenuList[] = [];
 
+	id: number = 0;
+	name: string = '';
+	price: number = 0;
+	description: string = '';
+	rating: number = 0;
+
+	addFoodMenu() {if (this.name,this.id,this.price,this.rating,this.description.trim())
+		this.foodMenuList.push({
+			id: this.id,
+			name: this.name,
+			price: this.price,
+			description: this.description,
+			rating: this.rating,
+		});
+		this.id = 0;
+		this.name = '';
+		this.price = 0;
+		this.description = '';
+		this.rating = 0;
+	}
 }
