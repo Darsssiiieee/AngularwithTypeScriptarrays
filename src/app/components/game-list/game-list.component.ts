eda303278678513178f51020755f686a5c7ca196
Darsssiiieee <darsie040@gmail.com>
2024-10-04 05:15:44 +0800

    implementation of game list

--- src/app/components/game-list/game-list.component.ts
+++ src/app/components/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import GameList from '../interface/game-list';
 
 @Component({
   selector: 'app-game-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './game-list.component.css'
 })
 export class GameListComponent {
+  games: GameList[] = [];
 
+	id: string = '';
+	name: string = '';
+	description: string = '';
+	platform: string = '';
+
+	addGame(): void { if (this.name,this.description,this.platform.trim())
+		this.games.push({
+			id: Number(this.id),
+			name: this.name,
+			description: this.description,
+			platform: this.platform,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.description = '';
+		this.platform = '';
+	}
 }
