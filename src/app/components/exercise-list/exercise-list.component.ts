5c534717cee83446578747686e2a2c206f635f1c
Darsssiiieee <darsie040@gmail.com>
2024-10-04 03:39:32 +0800

    implemetation of exercise list

--- src/app/components/exercise-list/exercise-list.component.ts
+++ src/app/components/exercise-list/exercise-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ExerciseList from '../interface/exercise-list';
 
 @Component({
   selector: 'app-exercise-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './exercise-list.component.css'
 })
 export class ExerciseListComponent {
+  exercises: ExerciseList[] = [];
 
+	name: string = '';
+	sets: number = 0;
+	reps: number = 0;
+	weight: number = 0;
+
+	addExercise() {if (this.sets,this.reps,this.weight,this.name.trim())
+		this.exercises.push({
+			name: this.name,
+			sets: this.sets,
+			reps: this.reps,
+			weight: this.weight,
+		});
+
+		this.name = '';
+		this.sets = 0;
+		this.reps = 0;
+		this.weight = 0;
+	}
 }
