d2d24c4ac8de13f7a9523eb044ffde41ad303abe
Darsssiiieee <darsie040@gmail.com>
2024-10-03 22:06:22 +0800

    implementation of animal list

--- src/app/components/animal-list/animal-list.component.ts
+++ src/app/components/animal-list/animal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Animals from '../interface/animals-list';
 
 @Component({
   selector: 'app-animal-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './animal-list.component.css'
 })
 export class AnimalListComponent {
+  animals: Animals[] = [];
 
+	id: string = '';
+	name: string = '';
+	origin: string = '';
+
+	addAnimal(): void { if (this.id,this.name,this.origin.trim())
+		this.animals.push({
+			id: Number(this.id),
+			name: this.name,
+			origin: this.origin,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.origin = '';
+	}
 }
