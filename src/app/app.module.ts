1d41d3bae0fb7817fda74ead4ef9290d57fcfb5a
Darsssiiieee <darsie040@gmail.com>
2024-10-03 21:48:18 +0800

    implementation of accessory list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -110,7 +111,8 @@ import { LibraryListComponent } from './components/library-list/library-list.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/accessory-list/accessory-list.component.ts
+++ src/app/components/accessory-list/accessory-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import AccessoryList from '../interface/accessory-list';
 
 @Component({
   selector: 'app-accessory-list',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './accessory-list.component.css'
 })
 export class AccessoryListComponent {
+  accessoryList: AccessoryList[] = [];
 
+	name: string = '';
+	description: string = '';
+	type: string = '';
+	price: number = 0;
+	category: string = '';
+
+	addAccessory() {if (this.name,this.description,this.type,this.price,this.category.trim())
+		this.accessoryList.push({
+      
+			name: this.name,
+			description: this.description,
+			type: this.type,
+			price: this.price,
+			category: this.category,
+		});
+
+		this.name = '';
+		this.description = '';
+		this.type = '';
+		this.price = 0;
+		this.category = '';
+	}
 }
