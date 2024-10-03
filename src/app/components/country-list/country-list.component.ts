3f1ddc1de14fbd1441f02a10ce9002de4cacc2a5
Darsssiiieee <darsie040@gmail.com>
2024-10-04 00:45:24 +0800

    implementation of country list

--- src/app/components/country-list/country-list.component.ts
+++ src/app/components/country-list/country-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Country from '../interface/country-list';
 
 @Component({
   selector: 'app-country-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './country-list.component.css'
 })
 export class CountryListComponent {
+  countries: Country[] = [];
 
+	id: string = '';
+	name: string = '';
+	capital: string = '';
+	population: string = '';
+
+	addCountry(): void { if (this.name,this.id,this.population,this.capital.trim())
+		this.countries.push({
+			id: Number(this.id),
+			name: this.name,
+			capital: this.capital,
+			population: Number(this.population),
+		});
+
+		this.id = '';
+		this.name = '';
+		this.capital = '';
+		this.population = '';
+	}
 }
