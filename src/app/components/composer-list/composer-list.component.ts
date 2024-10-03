90395e394efc6b79e6b0224d76496cec8b96ed78
Darsssiiieee <darsie040@gmail.com>
2024-10-04 02:28:05 +0800

    implementation of composer list

--- src/app/components/composer-list/composer-list.component.ts
+++ src/app/components/composer-list/composer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ComposerList from '../interface/composer-list';
 
 @Component({
   selector: 'app-composer-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './composer-list.component.css'
 })
 export class ComposerListComponent {
+  composers: ComposerList[] = [];
 
+	name: string = '';
+	popularMusic: string = '';
+	genre: string = '';
+	country: string = '';
+
+	addComposer() { if (this.name,this.popularMusic,this.genre,this.country.trim())
+		this.composers.push({
+			name: this.name,
+			popularMusic: this.popularMusic,
+			genre: this.genre,
+			country: this.country,
+		});
+
+		this.name = '';
+		this.popularMusic = '';
+		this.genre = '';
+		this.country = '';
+	}
 }
