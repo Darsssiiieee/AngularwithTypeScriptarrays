50ccad125f695b1ef81dbe3797a2a0b8ae419ecd
Darsssiiieee <darsie040@gmail.com>
2024-10-04 05:49:01 +0800

    implementation of inventory list

--- src/app/components/inventory-list/inventory-list.component.ts
+++ src/app/components/inventory-list/inventory-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import InventoryList from '../interface/inventory-list';
 
 @Component({
   selector: 'app-inventory-list',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './inventory-list.component.css'
 })
 export class InventoryListComponent {
+	inventoryList: InventoryList[] = [];
 
+	id: number = 0;
+	name: string = '';
+	quantity: number = 0;
+	price: number = 0;
+
+	addNewInventory() { if (this.id,this.quantity,this.price,this.name.trim())
+		this.inventoryList.push({
+			id: this.id,
+			name: this.name,
+			quantity: this.quantity,
+			price: this.price,
+		});
+		this.id = 0;
+		this.name = '';
+		this.quantity = 0;
+		this.price = 0;
+	}
 }
