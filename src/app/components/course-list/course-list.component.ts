dd819dfbef80047d8d3a152cc18983974e522691
Darsssiiieee <darsie040@gmail.com>
2024-10-04 01:56:33 +0800

    implementation of course list

--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CourseList } from '../interface/course-list';
 
 @Component({
   selector: 'app-course-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
+  courses: CourseList[] = [];
 
+	id: string = '';
+	name: string = '';
+	teacher: string = '';
+	credit: number = 0;
+
+	addCourse(): void { if (this.name,this.teacher,this.credit,this.id.trim())
+		this.courses.push({
+			id: this.id,
+			name: this.name,
+			teacher: this.teacher,
+			credit: this.credit,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.teacher = '';
+		this.credit = 0;
+	}
 }
