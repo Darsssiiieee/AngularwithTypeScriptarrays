b9280e5f743b30e4981001cb448eb56f623c7020
Darsssiiieee <darsie040@gmail.com>
2024-10-04 16:23:06 +0800

    implementation of student list

--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import StudentList from '../interface/student-list';
 
 @Component({
   selector: 'app-student-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './student-list.component.css'
 })
 export class StudentListComponent {
+  students: StudentList[] = [];
 
+	id: string = '';
+	name: string = '';
+	grade: string = '';
+	age: number = 0;
+
+	addStudent(): void { if (this.id,this.age,this.grade,this.name.trim())
+		this.students.push({
+			id: this.id,
+			name: this.name,
+			grade: this.grade,
+			age: this.age,
+		});
+
+		this.age = 0;
+		this.grade = '';
+		this.id = '';
+		this.name = '';
+	}
 }
