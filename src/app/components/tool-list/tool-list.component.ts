470c9cb5018f642b195e80425bcdba6f899d5098
Darsssiiieee <darsie040@gmail.com>
2024-10-04 16:46:05 +0800

    implementation of tool list

--- src/app/components/tool-list/tool-list.component.ts
+++ src/app/components/tool-list/tool-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import ToolList from '../interface/tool-list';
 
 @Component({
   selector: 'app-tool-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './tool-list.component.css'
 })
 export class ToolListComponent {
+  tools: ToolList[] = [];
 
+	id: string = '';
+	name: string = '';
+	description: string = '';
+	price: string = '';
+	quantity: string = '';
+
+	addTool(): void { if (this.name,this.id,this.price,this.quantity,this.description.trim())
+		this.tools.push({
+			id: Number(this.id),
+			name: this.name,
+			description: this.description,
+			price: Number(this.price),
+			quantity: Number(this.quantity),
+		});
+
+		this.id = '';
+		this.name = '';
+		this.description = '';
+		this.price = '';
+		this.quantity = '';
+	}
 }
