f113dd43c45319e15d05ed3de790a0daf901ea75
Darsssiiieee <darsie040@gmail.com>
2024-10-04 15:57:54 +0800

    implementation of sports list

--- src/app/components/sports-list/sports-list.component.ts
+++ src/app/components/sports-list/sports-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Sports from '../interface/sports-list';
 
 @Component({
   selector: 'app-sports-list',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './sports-list.component.css'
 })
 export class SportsListComponent {
+  sports: Sports[] = [];
+
+	id: string = '';
+	name: string = '';
+	team: string = '';
+
+	addSport(): void { if (this.name,this.team,this.id.trim())
+		this.sports.push({
+			id: Number(this.id),
+			name: this.name,
+			team: this.team,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.team = '';
+	}
 
 }
