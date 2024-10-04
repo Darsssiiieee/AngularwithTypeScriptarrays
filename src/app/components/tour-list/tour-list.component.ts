97642b9691da7be00587511c575392c52490263f
Darsssiiieee <darsie040@gmail.com>
2024-10-04 16:57:47 +0800

    implementation of tour list

--- src/app/components/tour-list/tour-list.component.ts
+++ src/app/components/tour-list/tour-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import TourList from '../interface/tour-list';
 
 @Component({
   selector: 'app-tour-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './tour-list.component.css'
 })
 export class TourListComponent {
+	tours: TourList[] = [];
 
+	tourDate: string = '';
+	tourName: string = '';
+	tourLocation: string = '';
+	tourPrice: number = 0;
+	tourDuration: string = '';
+
+	addTour() { if (this.tourName,this.tourDate,this.tourPrice,this.tourDuration,this.tourLocation.trim())
+		this.tours.push({
+			tourDate: this.tourDate,
+			tourName: this.tourName,
+			tourLocation: this.tourLocation,
+			tourPrice: this.tourPrice,
+			tourDuration: this.tourDuration,
+		});
+
+		this.tourDate = '';
+		this.tourName = '';
+		this.tourLocation = '';
+		this.tourPrice = 0;
+		this.tourDuration = '';
+	}
 }
