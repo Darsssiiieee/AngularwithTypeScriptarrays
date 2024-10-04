ce394c0263e6421eb00d3dd4379ee56067b5d7ac
Darsssiiieee <darsie040@gmail.com>
2024-10-04 13:45:42 +0800

    implementation of painting list

--- src/app/components/painting-list/painting-list.component.ts
+++ src/app/components/painting-list/painting-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import PaintingList from '../interface/painting-list';
 
 @Component({
   selector: 'app-painting-list',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './painting-list.component.css'
 })
 export class PaintingListComponent {
+  paintingList: PaintingList[] = [];
 
+	title: string = '';
+	artist: string = '';
+	year: number = 0;
+	price: number = 0;
+
+	addPainting() { if (this.year,this.price,this.title,this.artist.trim())
+		this.paintingList.push({
+			title: this.title,
+			artist: this.artist,
+			year: this.year,
+			price: this.price,
+		});
+		this.title = '';
+		this.artist = '';
+		this.year = 0;
+		this.price = 0;
+	}
 }
