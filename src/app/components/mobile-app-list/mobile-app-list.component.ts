624b2d0e4d5bb6844fe41502675ac65d0087e790
Darsssiiieee <darsie040@gmail.com>
2024-10-04 13:02:04 +0800

    implementation of mobile app list

--- src/app/components/mobile-app-list/mobile-app-list.component.ts
+++ src/app/components/mobile-app-list/mobile-app-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import MobileAppList from '../interface/mobile-app-list';
 
 @Component({
   selector: 'app-mobile-app-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './mobile-app-list.component.css'
 })
 export class MobileAppListComponent {
+  mobileApps: MobileAppList[] = [];
 
+	name: string = '';
+	description: string = '';
+	price: number = 0;
+	rating: number = 0;
+
+	addMobileApp() { if (this.name,this.price,this.rating,this.description.trim())
+		this.mobileApps.push({
+			name: this.name,
+			description: this.description,
+			price: this.price,
+			rating: this.rating,
+		});
+
+		this.name = '';
+		this.description = '';
+		this.price = 0;
+		this.rating = 0;
+	}
 }
