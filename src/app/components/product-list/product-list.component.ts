f6c6ee85e52dbf02ed6fb8ebb4444b08d0a986fb
Darsssiiieee <darsie040@gmail.com>
2024-10-04 14:48:29 +0800

    implementation of product list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Product from '../interface/product-list';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  products: Product[] = [];
 
+	id: string = '';
+	name: string = '';
+	price: number = 0;
+
+	addProduct(): void { if (this.id,this.price,this.name.trim())
+		this.products.push({
+			id: Number(this.id),
+			name: this.name,
+			price: this.price,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.price = 0;
+	}
 }
