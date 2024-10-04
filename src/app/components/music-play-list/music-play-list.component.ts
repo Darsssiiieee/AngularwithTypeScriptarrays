7ac5b22d683cc7c6ec84dbd459ce736d06e1f3be
Darsssiiieee <darsie040@gmail.com>
2024-10-04 13:35:25 +0800

    iplementation of music play list

--- src/app/components/music-play-list/music-play-list.component.ts
+++ src/app/components/music-play-list/music-play-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import MusicPlayList from '../interface/music-play-list';
 
 @Component({
   selector: 'app-music-play-list',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './music-play-list.component.css'
 })
 export class MusicPlayListComponent {
+  musicPlayList: MusicPlayList[] = [];
 
+	id: string = '';
+	name: string = '';
+	artist: string = '';
+	album: string = '';
+	duration: string = '';
+	genre: string = '';
+	year: string = '';
+
+	addMusicPlayList(): void { if (this.name,this.id,this.year,this.duration,this.genre,this.album,this.artist.trim())
+		this.musicPlayList.push({
+			id: Number(this.id),
+			name: this.name,
+			artist: this.artist,
+			album: this.album,
+			duration: this.duration,
+			genre: this.genre,
+			year: this.year,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.artist = '';
+		this.album = '';
+		this.duration = '';
+		this.genre = '';
+		this.year = '';
+	}
 }
