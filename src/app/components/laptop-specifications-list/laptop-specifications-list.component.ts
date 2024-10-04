a4d4eb08de98f6de211478c54a5565b0e7f9a841
Darsssiiieee <darsie040@gmail.com>
2024-10-04 12:10:32 +0800

    implementation of laptop specifications list

--- src/app/components/laptop-specifications-list/laptop-specifications-list.component.ts
+++ src/app/components/laptop-specifications-list/laptop-specifications-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import LaptopSpecs from '../interface/laptop-specs-list';
 
 @Component({
   selector: 'app-laptop-specifications-list',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './laptop-specifications-list.component.css'
 })
 export class LaptopSpecificationsListComponent {
+  laptops: LaptopSpecs[] = [];
 
+	model: string = '';
+	processor: string = '';
+	ram: string = '';
+	storage: string = '';
+	display: string = '';
+	graphics: string = '';
+
+	addLaptop() {if (this.model,this.processor,this.ram,this.storage,this.display,this.graphics.trim())
+		this.laptops.push({
+			model: this.model,
+			specs: {
+				processor: this.processor,
+				ram: this.ram,
+				storage: this.storage,
+				display: this.display,
+				graphics: this.graphics,
+			},
+		});
+		this.model = '';
+		this.processor = '';
+		this.ram = '';
+		this.storage = '';
+		this.display = '';
+		this.graphics = '';
+	}
 }
