5ac61d37fcc39d1318533a40d8ddd758a99a54e2
Darsssiiieee <darsie040@gmail.com>
2024-10-04 15:48:23 +0800

    implementation of software list

--- src/app/components/software-list/software-list.component.ts
+++ src/app/components/software-list/software-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import SoftwareList from '../interface/software-list';
 
 @Component({
   selector: 'app-software-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './software-list.component.css'
 })
 export class SoftwareListComponent {
+  softwareList: SoftwareList[] = [];
 
+	id: string = '';
+	name: string = '';
+	description: string = '';
+	platform: string = '';
+
+	addSoftware(): void { if (this.name,this.id,this.description,this.platform.trim())
+		this.softwareList.push({
+			id: Number(this.id),
+			name: this.name,
+			description: this.description,
+			platform: this.platform,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.description = '';
+		this.platform = '';
+	}
 }
