406baa93c62e254b663a39fa54a5782349f331a0
Darsssiiieee <darsie040@gmail.com>
2024-10-04 12:50:48 +0800

    implementation of meal plan list

--- src/app/components/meal-plan-list/meal-plan-list.component.ts
+++ src/app/components/meal-plan-list/meal-plan-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import MealPlanList from '../interface/meal-plan-list';
 
 @Component({
   selector: 'app-meal-plan-list',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './meal-plan-list.component.css'
 })
 export class MealPlanListComponent {
+  meals: MealPlanList[] = [];
 
+	name: string = '';
+	breakfast: string = '';
+	lunch: string = '';
+	dinner: string = '';
+	snack: string = '';
+	drink: string = '';
+
+	addMealPlan() { if (this.name,this.breakfast,this.lunch,this.dinner,this.snack,this.drink.trim())
+		this.meals.push({
+			name: this.name,
+			breakfast: this.breakfast,
+			lunch: this.lunch,
+			dinner: this.dinner,
+			snack: this.snack,
+			drink: this.drink,
+		});
+
+		this.name = '';
+		this.breakfast = '';
+		this.lunch = '';
+		this.dinner = '';
+		this.snack = '';
+		this.drink = '';
+	}
 }
