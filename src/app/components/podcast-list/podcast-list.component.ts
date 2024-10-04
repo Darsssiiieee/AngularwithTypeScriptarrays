9d96103c493bc3a15a9a2a5348ff05492ad08e43
Darsssiiieee <darsie040@gmail.com>
2024-10-04 14:15:48 +0800

    implementation of podcast list

--- src/app/components/podcast-list/podcast-list.component.ts
+++ src/app/components/podcast-list/podcast-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import PodcastList from '../interface/podcast-list';
 
 @Component({
   selector: 'app-podcast-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './podcast-list.component.css'
 })
 export class PodcastListComponent {
+	podcasts: PodcastList[] = [];
 
+	name: string = '';
+	host: string = '';
+	genre: string = '';
+	episodeNumber: number = 0;
+
+	addPodcast() { if (this.name,this.host,this.episodeNumber,this.genre.trim())
+		this.podcasts.push({
+			name: this.name,
+			host: this.host,
+			genre: this.genre,
+			episodeNumber: this.episodeNumber,
+		});
+
+		this.name = '';
+		this.host = '';
+		this.genre = '';
+		this.episodeNumber = 0;
+	}
 }
