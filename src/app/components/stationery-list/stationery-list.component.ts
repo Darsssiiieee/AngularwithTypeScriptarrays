a237f93bf1d9005439366ace3f868f11e2ee24e0
Darsssiiieee <darsie040@gmail.com>
2024-10-04 16:10:48 +0800

    implementation of stationery list

--- src/app/components/stationery-list/stationery-list.component.ts
+++ src/app/components/stationery-list/stationery-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import StationeryList from '../interface/stationery-list';
 
 @Component({
   selector: 'app-stationery-list',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './stationery-list.component.css'
 })
 export class StationeryListComponent {
+  stationeryList: StationeryList[] = [];
 
+	id: number = 0;
+	name: string = '';
+	quantity: number = 0;
+	price: number = 0;
+
+	addNewStationery() {if (this.id,this.quantity,this.price,this.name.trim())
+		this.stationeryList.push({
+			id: this.id,
+			name: this.name,
+			quantity: this.quantity,
+			price: this.price,
+		});
+		this.id = 0;
+		this.name = '';
+		this.quantity = 0;
+		this.price = 0;
+	}
 }
