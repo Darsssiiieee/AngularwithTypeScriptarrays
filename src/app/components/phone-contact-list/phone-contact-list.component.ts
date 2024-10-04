ccbdc2d73bec6b55f30e0700dd0d49fc2b02c1a3
Darsssiiieee <darsie040@gmail.com>
2024-10-04 14:00:56 +0800

    implementation of phone contact list

--- src/app/components/phone-contact-list/phone-contact-list.component.ts
+++ src/app/components/phone-contact-list/phone-contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import PhoneContactList from '../interface/phone-contact-list';
 
 @Component({
   selector: 'app-phone-contact-list',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './phone-contact-list.component.css'
 })
 export class PhoneContactListComponent {
+  phoneContactList: PhoneContactList[] = [];
 
+	id: string = '';
+	name: string = '';
+	email: string = '';
+	phone: string = '';
+
+	addPhoneContact(): void {if (this.name,this.id,this.email,this.phone.trim())
+		this.phoneContactList.push({
+			id: Number(this.id),
+			name: this.name,
+			email: this.email,
+			phone: this.phone,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.email = '';
+		this.phone = '';
+	}
 }
