7622fdbcfa8ed5f1a9713973cf1c5eb3c2705d39
Darsssiiieee <darsie040@gmail.com>
2024-10-04 17:22:10 +0800

    implementation of vegetable list

--- src/app/components/vegetable-list/vegetable-list.component.ts
+++ src/app/components/vegetable-list/vegetable-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Vegetables from '../interface/vegetables-list';
 
 @Component({
   selector: 'app-vegetable-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './vegetable-list.component.css'
 })
 export class VegetableListComponent {
+  vegetables: Vegetables[] = [];
 
+	id: string = '';
+	name: string = '';
+	price: string = '';
+
+	addVegetable(): void { if (this.price,this.id,this.name.trim())
+		this.vegetables.push({
+			id: Number(this.id),
+			name: this.name,
+			price: Number(this.price),
+		});
+
+		this.id = '';
+		this.name = '';
+		this.price = '';
+	}
 }
