d9cf9372ccef1d4dd000ca30204cecccf99d81f2
Darsssiiieee <darsie040@gmail.com>
2024-10-04 17:39:11 +0800

    implementation of video list

--- src/app/components/video-list/video-list.component.ts
+++ src/app/components/video-list/video-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import VideoList from '../interface/video-list';
 
 @Component({
   selector: 'app-video-list',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './video-list.component.css'
 })
 export class VideoListComponent {
+  videoList: VideoList[] = [];
 
+	name: string = '';
+	description: string = '';
+	url: string = '';
+	rating: number = 0;
+	category: string = '';
+
+	addVideo() { if (this.name,this.description,this.url,this.rating,this.category.trim())
+		this.videoList.push({
+			name: this.name,
+			description: this.description,
+			url: this.url,
+			rating: this.rating,
+			category: this.category,
+		});
+		this.name = '';
+		this.description = '';
+		this.url = '';
+		this.rating = 0;
+		this.category = '';
+	}
 }
