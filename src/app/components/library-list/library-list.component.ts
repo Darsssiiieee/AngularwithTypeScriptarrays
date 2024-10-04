c51e2841d4f8aacf8fd436d69d3d2251f41be680
Darsssiiieee <darsie040@gmail.com>
2024-10-04 12:37:49 +0800

    implementation of library list

--- src/app/components/library-list/library-list.component.ts
+++ src/app/components/library-list/library-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import LibraryList from '../interface/library-list';
 
 @Component({
   selector: 'app-library-list',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './library-list.component.css'
 })
 export class LibraryListComponent {
+  libraries: LibraryList[] = [];
 
+	libraryName: string = '';
+	programmingLanguage: string = '';
+	description: string = '';
+	developedBy: string = '';
+	firstRelease: string = '';
+	latestRelease: string = '';
+
+	addLibrary() { if (this.libraryName,this.programmingLanguage,this.description,this.developedBy,this.firstRelease,this.latestRelease.trim())
+		this.libraries.push({
+			libraryName: this.libraryName,
+			programmingLanguage: this.programmingLanguage,
+			description: this.description,
+			developedBy: this.developedBy,
+			firstRelease: this.firstRelease,
+			latestRelease: this.latestRelease,
+		});
+
+		this.libraryName = '';
+		this.programmingLanguage = '';
+		this.description = '';
+		this.developedBy = '';
+		this.firstRelease = '';
+		this.latestRelease = '';
+	}
 }
