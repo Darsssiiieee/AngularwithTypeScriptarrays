4ad2a66c78123349511f45da7525bd442a3418b9
Darsssiiieee <darsie040@gmail.com>
2024-10-04 12:22:35 +0800

    implementation of lecture list

--- src/app/components/lecture-list/lecture-list.component.ts
+++ src/app/components/lecture-list/lecture-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import LectureList from '../interface/lecture-list';
 
 @Component({
   selector: 'app-lecture-list',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './lecture-list.component.css'
 })
 export class LectureListComponent {
+	lectureList: LectureList[] = [];
 
+	id: number = 0;
+	name: string = '';
+	duration: number = 0;
+	instructor: string = '';
+
+	addLecture() { if (this.name,this.id,this.duration,this.instructor.trim())
+		this.lectureList.push({
+			id: this.id,
+			name: this.name,
+			duration: this.duration,
+			instructor: this.instructor,
+		});
+		this.id = 0;
+		this.name = '';
+		this.duration = 0;
+		this.instructor = '';
+	}
 }
