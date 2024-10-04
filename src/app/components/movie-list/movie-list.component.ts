8dbf4aad894c3ffc55cd51bb895123132578e9ff
Darsssiiieee <darsie040@gmail.com>
2024-10-04 13:13:54 +0800

    implementation of movie list

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import MovieList from '../interface/movie-list';
 
 @Component({
   selector: 'app-movie-list',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './movie-list.component.css'
 })
 export class MovieListComponent {
+  movies: MovieList[] = [];
 
+	id: string = '';
+	name: string = '';
+	director: string = '';
+	year: number = 0;
+	rating: number = 0;
+
+	addMovie(): void { if (this.name,this.rating,this.director,this.year,this.id.trim())
+		this.movies.push({
+			id: this.id,
+			name: this.name,
+			director: this.director,
+			year: this.year,
+			rating: this.rating,
+		});
+
+		this.id = '';
+		this.name = '';
+		this.director = '';
+		this.year = 0;
+		this.rating = 0;
+	}
 }
