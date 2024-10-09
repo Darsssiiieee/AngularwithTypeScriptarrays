f24d6640fe4f84fdf2d47c1bfe785e545613982c
Darsssiiieee <darsie040@gmail.com>
2024-10-10 03:44:26 +0800

    added building list service

--- src/app/components/angular-services/building-list-service/building-list-services.service.ts
+++ src/app/components/angular-services/building-list-service/building-list-services.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import BuildingList from '../../interface/building-list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BuildingListServicesService {
-
-  constructor() { }
+  private buildings: BuildingList[] = [];
+  
+	constructor() {}
+	addBuilding(
+		name: string,
+		floors: number,
+		rooms: number,
+		occupants: number,
+    address: string,
+	): void {
+		this.buildings.push({
+      name: name,
+      floors: floors,
+      rooms: rooms,
+      occupants: occupants,
+      address: address,
+    });
+	}
+	getBuildings(): BuildingList[] {
+		return this.buildings;
+	}
 }
--- src/app/components/building-list/building-list.component.ts
+++ src/app/components/building-list/building-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import BuildingList from '../interface/building-list';
+import { BuildingListServicesService } from '../angular-services/building-list-service/building-list-services.service';
 
 @Component({
   selector: 'app-building-list',
@@ -7,7 +8,10 @@ import BuildingList from '../interface/building-list';
   styleUrl: './building-list.component.css'
 })
 export class BuildingListComponent {
-  buildingList: BuildingList[] = [];
+	buildingList = [] as BuildingList[];
+	constructor(private buildingService: BuildingListServicesService) {
+		this.buildingList = this.buildingService.getBuildings();
+	}
 
 	name: string = '';
 	address: string = '';
@@ -16,13 +20,13 @@ export class BuildingListComponent {
 	occupants: number = 0;
 
 	addBuilding() { if (this.name,this.floors,this.rooms,this.occupants,this.address.trim())
-		this.buildingList.push({
-			name: this.name,
-			address: this.address,
-			floors: this.floors,
-			rooms: this.rooms,
-			occupants: this.occupants,
-		});
+		this.buildingService.addBuilding(
+			this.name,
+			this.floors,
+			this.rooms,
+			this.occupants,
+			this.address
+		);
 
 		this.name = '';
 		this.address = '';
