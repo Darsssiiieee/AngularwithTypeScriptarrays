e68be09f74cae533c1da87844258b60a4d1d4f90
Darsssiiieee <darsie040@gmail.com>
2024-10-10 04:31:03 +0800

    added classroom list service

--- src/app/components/angular-services/classroom-list-service/classroom-list-services.service.ts
+++ src/app/components/angular-services/classroom-list-service/classroom-list-services.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import ClassroomList from '../../interface/classroom-list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassroomListServicesService {
-
-  constructor() { }
+  private classrooms: ClassroomList[] = [];
+	constructor() {}
+  
+	addClassroom(
+id: number, name: string, teacher: string, studentCount: number, section: string,
+	): void {
+		this.classrooms.push({
+      id: id,
+      name: name,
+      teacher: teacher,
+      studentCount: studentCount,
+      section: ''
+    });
+	}
+	getClassrooms(): ClassroomList[] {
+		return this.classrooms;
+	}
 }
--- src/app/components/classroom-list/classroom-list.component.ts
+++ src/app/components/classroom-list/classroom-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import ClassroomList from '../interface/classroom-list';
+import { ClassroomListServicesService } from '../angular-services/classroom-list-service/classroom-list-services.service';
 
 @Component({
   selector: 'app-classroom-list',
@@ -7,23 +8,28 @@ import ClassroomList from '../interface/classroom-list';
   styleUrl: './classroom-list.component.css'
 })
 export class ClassroomListComponent {
-  classroomList: ClassroomList[] = [];
-
+	classroomList = [] as ClassroomList[];
+	constructor(private classroomService: ClassroomListServicesService) {
+		this.classroomList = this.classroomService.getClassrooms();
+	}
 	id: number = 0;
 	section: string = '';
 	teacher: string = '';
 	studentCount: number = 0;
+	name: string = '';
 
-	addNewClassroom() { if (this.section,this.id,this.studentCount,this.teacher.trim())
-		this.classroomList.push({
-			id: this.id,
-			section: this.section,
-			teacher: this.teacher,
-			studentCount: this.studentCount,
-		});
+	addNewClassroom() { if (this.name,this.section,this.id,this.studentCount,this.teacher.trim())
+		this.classroomService.addClassroom(
+			this.id,
+			this.name,
+			this.teacher,
+			this.studentCount,
+			this.section,
+		);
 		this.id = 0;
 		this.section= '';
 		this.teacher = '';
 		this.studentCount = 0;
+		this.name = '';
 	}
 }
