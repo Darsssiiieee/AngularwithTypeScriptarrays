841c748b04936eba93ee309a40af898d4b0234bb
Darsssiiieee <darsie040@gmail.com>
2024-10-10 16:46:25 +0800

    added language list service

--- src/app/components/angular-services/language-list-service/language-list-services.service.ts
+++ src/app/components/angular-services/language-list-service/language-list-services.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import LanguageList from '../../interface/language-list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageListServicesService {
+  private languages: LanguageList[] = [];
 
-  constructor() { }
+	constructor() {}
+
+	addLanguage(
+		id: number,
+		name: string,
+		description: string,
+		origin: string,
+	): void {
+		this.languages.push({
+			id: id,
+			name: name,
+			description: description,
+			origin: origin,
+		});
+	}
+
+	getLanguages(): LanguageList[] {
+		return this.languages;
+	}
 }
--- src/app/components/language-list/language-list.component.ts
+++ src/app/components/language-list/language-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import LanguageList from '../interface/language-list';
+import { LanguageListServicesService } from '../angular-services/language-list-service/language-list-services.service';
 
 @Component({
   selector: 'app-language-list',
@@ -7,22 +8,25 @@ import LanguageList from '../interface/language-list';
   styleUrl: './language-list.component.css'
 })
 export class LanguageListComponent {
-  languages: LanguageList[] = [];
+	languageList = [] as LanguageList[];
+	constructor(private languageService: LanguageListServicesService) {
+		this.languageList = this.languageService.getLanguages();
+	}
 
-	id: string = '';
+	id: number = 0;
 	name: string = '';
 	description: string = '';
 	origin: string = '';
 
 	addLanguage(): void {if (this.name,this.id,this.description,this.origin.trim())
-		this.languages.push({
-			id: Number(this.id),
-			name: this.name,
-			description: this.description,
-			origin: this.origin,
-		});
+		this.languageService.addLanguage(
+			this.id,
+			this.name,
+			this.description,
+			this.origin,
+		);
 
-		this.id = '';
+		this.id = 0;
 		this.name = '';
 		this.description = '';
 		this.origin = '';
