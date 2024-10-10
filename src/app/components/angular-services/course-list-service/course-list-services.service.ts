9d2d65440b8a590a198e6435c761365ab2cb6b82
Darsssiiieee <darsie040@gmail.com>
2024-10-10 15:03:48 +0800

    added course list service

--- src/app/components/angular-services/course-list-service/course-list-services.service.ts
+++ src/app/components/angular-services/course-list-service/course-list-services.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { CourseList } from '../../interface/course-list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseListServicesService {
+	private courses: CourseList[] = [];
 
-  constructor() { }
+	constructor() {}
+
+	addCourse(id: string, name: string, teacher: string, credit: number): void {
+		this.courses.push({
+			id: id,
+			name: name,
+			teacher: teacher,
+			credit: credit,
+		});
+	}
+
+	getCourses(): CourseList[] {
+		return this.courses;
+	}
 }
--- src/app/components/course-list/course-list.component.ts
+++ src/app/components/course-list/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CourseList } from '../interface/course-list';
+import { CourseListServicesService } from '../angular-services/course-list-service/course-list-services.service';
 
 @Component({
   selector: 'app-course-list',
@@ -7,20 +8,22 @@ import { CourseList } from '../interface/course-list';
   styleUrl: './course-list.component.css'
 })
 export class CourseListComponent {
-  courses: CourseList[] = [];
-
+	courses = [] as CourseList[];
+	constructor(private courseListService: CourseListServicesService) {
+		this.courses = this.courseListService.getCourses();
+	}
 	id: string = '';
 	name: string = '';
 	teacher: string = '';
 	credit: number = 0;
 
 	addCourse(): void { if (this.name,this.teacher,this.credit,this.id.trim())
-		this.courses.push({
-			id: this.id,
-			name: this.name,
-			teacher: this.teacher,
-			credit: this.credit,
-		});
+		this.courseListService.addCourse(
+			this.id,
+			this.name,
+			this.teacher,
+			this.credit,
+		);
 
 		this.id = '';
 		this.name = '';
