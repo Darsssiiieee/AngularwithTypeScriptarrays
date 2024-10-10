d5264fa15d0922067843cf5c918a9e512ae46de9
Darsssiiieee <darsie040@gmail.com>
2024-10-10 14:41:01 +0800

    added computer hardware list service

--- src/app/components/computer-hardware-list/computer-hardware-list.component.ts
+++ src/app/components/computer-hardware-list/computer-hardware-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import ComputerHardware from '../interface/computer-hardware-list';
+import { ComputerHardwareListServicesService } from '../angular-services/computer-hardware-list-service/computer-hardware-list-services.service';
 
 @Component({
   selector: 'app-computer-hardware-list',
@@ -7,20 +8,26 @@ import ComputerHardware from '../interface/computer-hardware-list';
   styleUrl: './computer-hardware-list.component.css'
 })
 export class ComputerHardwareListComponent {
-  computerHardware: ComputerHardware[] = [];
+	computerHardware = [] as ComputerHardware[];
 
+	constructor(
+		private computerHardwareListService: ComputerHardwareListServicesService,
+	) {
+		this.computerHardware =
+			this.computerHardwareListService.getComputerHardwares();
+	}
 	name: string = '';
 	location: string = '';
 	type: string = '';
 	price: number = 0;
 
 	addComputerHardware() { if (this.name,this.type,this.price,this.location.trim())
-		this.computerHardware.push({
-			name: this.name,
-			location: this.location,
-			type: this.type,
-			price: this.price,
-		});
+		this.computerHardwareListService.addComputerHardware(
+			this.name,
+			this.location,
+			this.type,
+			this.price,
+		);
 
 		this.name = '';
 		this.location = '';
