7ecb8a1a6af93ad0aa3b0c2d7430a60db11b809a
Darsssiiieee <darsie040@gmail.com>
2024-10-10 19:31:08 +0800

    added tool list service

--- src/app/components/angular-services/tool-list-service/tool-list-services.service.ts
+++ src/app/components/angular-services/tool-list-service/tool-list-services.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import ToolList from '../../interface/tool-list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToolListServicesService {
+  private tools: ToolList[] = [];
 
-  constructor() { }
+	constructor() {}
+
+	addTool(
+		id: number,
+		name: string,
+		description: string,
+		price: number,
+		quantity: number,
+	): void {
+		this.tools.push({
+			id: id,
+			name: name,
+			description: description,
+			price: price,
+			quantity: quantity,
+		});
+	}
+
+	getTools(): ToolList[] {
+		return this.tools;
+	}
 }
--- src/app/components/tool-list/tool-list.component.ts
+++ src/app/components/tool-list/tool-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import ToolList from '../interface/tool-list';
+import { ToolListServicesService } from '../angular-services/tool-list-service/tool-list-services.service';
 
 @Component({
   selector: 'app-tool-list',
@@ -7,27 +8,30 @@ import ToolList from '../interface/tool-list';
   styleUrl: './tool-list.component.css'
 })
 export class ToolListComponent {
-  tools: ToolList[] = [];
+	tools = [] as ToolList[];
+	constructor(private toolService: ToolListServicesService) {
+		this.tools = this.toolService.getTools();
+	}
 
-	id: string = '';
+	id: number = 0;
 	name: string = '';
 	description: string = '';
-	price: string = '';
-	quantity: string = '';
+	price: number = 0;
+	quantity: number = 0;
 
 	addTool(): void { if (this.name,this.id,this.price,this.quantity,this.description.trim())
-		this.tools.push({
-			id: Number(this.id),
-			name: this.name,
-			description: this.description,
-			price: Number(this.price),
-			quantity: Number(this.quantity),
-		});
+		this.toolService.addTool(
+			this.id,
+			this.name,
+			this.description,
+			this.price,
+			this.quantity,
+		);
 
-		this.id = '';
+		this.id = 0;
 		this.name = '';
 		this.description = '';
-		this.price = '';
-		this.quantity = '';
+		this.price = 0;
+		this.quantity = 0;
 	}
 }
