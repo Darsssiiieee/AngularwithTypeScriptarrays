d17f7359237ed3679f02e4c2861582aaf12dd459
Darsssiiieee <darsie040@gmail.com>
2024-10-10 18:19:02 +0800

    added phone contact list service

--- src/app/components/angular-services/phone-contact-list-service/phone-contact-list-services.service.ts
+++ src/app/components/angular-services/phone-contact-list-service/phone-contact-list-services.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import PhoneContactList from '../../interface/phone-contact-list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhoneContactListServicesService {
+	private phoneContacts: PhoneContactList[] = [];
 
-  constructor() { }
+	constructor() {}
+
+	addPhoneContact(
+		id: number,
+		name: string,
+		email: string,
+		phone: string,
+	): void {
+		this.phoneContacts.push({
+			id: id,
+			name: name,
+			email: email,
+			phone: phone,
+		});
+	}
+
+	getPhoneContacts(): PhoneContactList[] {
+		return this.phoneContacts;
+	}
 }
--- src/app/components/phone-contact-list/phone-contact-list.component.ts
+++ src/app/components/phone-contact-list/phone-contact-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import PhoneContactList from '../interface/phone-contact-list';
+import { PhoneContactListServicesService } from '../angular-services/phone-contact-list-service/phone-contact-list-services.service';
 
 @Component({
   selector: 'app-phone-contact-list',
@@ -7,22 +8,24 @@ import PhoneContactList from '../interface/phone-contact-list';
   styleUrl: './phone-contact-list.component.css'
 })
 export class PhoneContactListComponent {
-  phoneContactList: PhoneContactList[] = [];
-
-	id: string = '';
+	phoneContactList = [] as PhoneContactList[];
+	constructor(private phoneContactListService: PhoneContactListServicesService) {
+		this.phoneContactList = this.phoneContactListService.getPhoneContacts();
+	}
+	id: number = 0;
 	name: string = '';
 	email: string = '';
 	phone: string = '';
 
 	addPhoneContact(): void {if (this.name,this.id,this.email,this.phone.trim())
-		this.phoneContactList.push({
-			id: Number(this.id),
-			name: this.name,
-			email: this.email,
-			phone: this.phone,
-		});
+		this.phoneContactListService.addPhoneContact(
+			this.id,
+			this.name,
+			this.email,
+			this.phone,
+		);
 
-		this.id = '';
+		this.id = 0;
 		this.name = '';
 		this.email = '';
 		this.phone = '';
